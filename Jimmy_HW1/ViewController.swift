a78f7593bd25b140f0029a3fd536184e35462c5e
Chin-hui Wang <chin-huiwang@chin-huide-macbook.local>
2016-07-10 12:15:02 +0800

    Initial Commit

---
+++ Jimmy_HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jimmy_HW1
+//
+//  Created by Chin-hui Wang on 2016/7/10.
+//  Copyright © 2016年 Chin-hui Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
