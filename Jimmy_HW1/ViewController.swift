7b8ef4f9720eff8d1f3778e7cdb35eda8d37074d
Jimmy_Wang <chin-huiwang@chinhuiemacbook.home>
2016-07-10 19:57:54 +0800

    add one button

--- Jimmy_HW1/ViewController.swift
+++ Jimmy_HW1/ViewController.swift
@@ -20,6 +20,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage(){
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello you, nice to meet you" , preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    
 }
 
